7e604b9c4672418d26a789f82fde5676153aec26
kmitcourses <support@kmitcourses.com>
2026-02-16 15:24:55 +0000

    Pushing final code

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello Codespace")
+// console.log("Hello Codespace");
+// const mysec= process.env.MYSEC;
+// console.log(mysec);
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from GitHub Codespaces');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
